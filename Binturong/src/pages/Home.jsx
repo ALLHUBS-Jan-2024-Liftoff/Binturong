6020ba153561e27f253e63c6b8d787454fe6471a
Travis <azai392@gmail.com>
2024-07-25 13:03:39 -0500

    Added a login button to the welcome/home screen

--- Binturong/src/pages/Home.jsx
+++ Binturong/src/pages/Home.jsx
@@ -1,7 +1,32 @@
 //Home page
+import React from 'react'
+import {useNavigate} from 'react-router-dom'
 
-const Home = () => {
-    return <h1>2Gether</h1>;
+const Home = (props) => {
+    const {loggedIn, userName} = props
+    const navigate = useNavigate()
+
+    const onButtonClick = () => {
+        //Button function will be added soon
+    }
+
+    return (
+        <div>
+            <div className = {'appName'}>
+                <h1>Welcome to 2gether!</h1>
+            </div>
+            <div>
+                <input
+                    className = {'loginButton'}
+                    type = "button"
+                    onClick = {onButtonClick}
+                    value = {logginIn ? 'Log out' : 'Log in'}
+                />
+                {loggedIn ? <div>Your username is {userName}</div> : <div />}
+            </div>
+        </div>
+
+    )
   };
   
   export default Home;
