abd7511fb400dab979341b030a9d4949fa8f9f4a
Joseph <Demicaster@gmail.com>
2024-08-01 13:29:23 -0500

    Added form saving still need to connect to backend

--- Binturong/src/Components/PostForm.jsx
+++ Binturong/src/Components/PostForm.jsx
@@ -4,6 +4,20 @@ import React,{useState} from 'react';
 
 function PostForm() {
 
+    const [postText,setPostText] = useState('')
+    const [geoTaggingCheck,setGeoTag] = useState('')
+    const [postFile,setFile]= useState('')
+
+    function savePost(e) {
+        e.preventDefault();
+
+        const post ={postText,geoTaggingCheck,postFile};
+        console.log(post)
+
+    }
+
+
+
     //setting state of the form to be hidden when PostForm first loads.
     const [showForm,setShowForm] = useState(false);
 
@@ -18,10 +32,35 @@ function PostForm() {
              {showForm &&(
             <div>
                 <form id="post-form">
-                    <textarea id="posttextbox" name= "posttext" rows= "5" cols="50">Text Goes here</textarea><br/>
-                    <label for ="geotagging">Turn on Geotagging?<br/><input type="checkbox" id="geotagging" name ="geotagging" value="GeoTagging?"></input></label><br/>
-                    <label for ="postfile">Upload files<input type ="file" id="postfile" name="postfile"></input></label>
-                    <button>Post</button>
+                    <textarea id="posttextbox"
+                     name= "posttext"
+                     value={postText}
+                      rows= "5"  cols="50"
+                      onChange={(e) => {setPostText(e.target.value)}}>
+                        Text Goes here
+                      </textarea><br/>
+
+                    <label for ="geotagging">Turn on Geotagging?<br/>
+                    <input type="checkbox"
+                    id="geotagging"
+                    name ="geotagging" 
+                    value={geoTaggingCheck}
+                    onChange={(e) => {setGeoTag(e.target.value)}}>
+                    
+                    </input>
+                    </label>
+                    <br/>
+
+                    <label for ="postfile">Upload files
+                        <input type ="file" 
+                        id="postfile" 
+                        name="postfile"
+                        value={postFile}
+                        onChange={(e) => {setFile(e.target.value)}}>
+                            </input>
+                            </label>
+
+                    <button onClick={savePost}>Post</button>
                 </form>
             </div>
             )} 
