c4b2d810ebf183be2eb1e4ffcdc8a8f54d5268df
Travis <azai392@gmail.com>
2024-08-11 02:56:46 -0500

    Auth filters done!

---
+++ binturong-api/src/main/java/com/binturong/demo/WebAppConfig.java
@@ -0,0 +1,18 @@
+package com.binturong.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebAppConfig implements WebMvcConfigurer {
+
+    // Allows app to access the filter by creating a spring-managed object.
+    @Bean
+    public AuthenticationFilter authenticationFilter() { return new AuthenticationFilter(); }
+
+    // Registers filter with spring container
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) { registry.addInterceptor( authenticationFilter() ); }
+}
