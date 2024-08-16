8e672e8d0cf8a8ee0ca85d52e26e4198871103db
Dallas Wyciskalla <dallas.wyciskalla@gmail.com>
2024-08-15 20:22:32 -0500

    added code to userProfile to fetch loyalty score

--- Binturong/src/pages/UserProfile.jsx
+++ Binturong/src/pages/UserProfile.jsx
@@ -1,6 +1,23 @@
 //Start of the User Profile page
 
 const UserProfile = () => {
+    const[loyaltyScore, setLoyaltyScore] = useState(0);
+
+    useEffect(() => {
+        const fetchLoyaltyScore = async() =>{
+            try {
+                const response = await fetch('http://localhost:8080/loyalty-score?user_id=1');
+                const data = await response.json();
+                setLoyaltyScore(data.total_score);
+            } catch(error){
+             console.error('Error fetching loyalty score:', error);
+
+          }
+
+        };
+
+    fetchLoyaltyScore();
+    }, []); // Empty dependency array means this useEffect runs only once, after the component mounts
 
 
 
@@ -9,16 +26,11 @@ const UserProfile = () => {
 
                     <h1>User Profile</h1>;
                     <h3>Thank you for your contribution!</h3>
-                    <h3>Your Loyalty Score is:  </h3>
+                    <h3>Your Loyalty Score is: {loyaltyScore} </h3>
 
 
                 </div>
-
-
     )
-
-
-
 
   };
   
